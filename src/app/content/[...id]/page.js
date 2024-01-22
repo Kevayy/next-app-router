1c4a67279226d2b1112235d802cf2e9015334ea8
Kevin van Bommel <kevin.van.bommel@2-control.nl>
2024-01-22 14:58:32 +0100

    Sectie 19 127. Catch all segments (fix 2)

--- src/app/content/[...id]/page.js
+++ src/app/content/[...id]/page.js
@@ -1,16 +1,17 @@
 
-function getContent(id, author) {
+function getContent(id, author){
     // ...
 }
-
+   
 export default function Content({params}) {
+   
     const [id, author] = params.id;
-
+   
     const content = getContent(id, author);
-    
+   
     return (
         <div>
-            Hello I am Content of:
+            Hello I am Content of: 
             <div>Id: {id}</div>
             <div>Author: {author}</div>
         </div>
