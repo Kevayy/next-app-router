a5bc402548d4db4cc39889a5ad0b9c0a5cb77497
Kevin van Bommel <kevin.van.bommel@2-control.nl>
2024-01-26 09:54:28 +0100

    Sectie 20 139. Hero Component

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
+import { Hero } from "@/components/Hero";
 import { BlogList } from "@/components/blogs/BlogList";
 import { PortfolioList } from "@/components/portfolios/PortfolioList";
-import Link from "next/link";
 
 export default async function Home() {
   return (
     <>
+      <Hero />
       <BlogList />
       <PortfolioList />
     </>
